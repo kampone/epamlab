a4152bf31f902e73531a9230bb9ae1564f6146e8
kampone <vladislavkaminski2806@gmail.com>
2015-07-16 11:57:20 +0300

    Spring MVC helloWorld controller created

---
+++ JavaLab/2Task/news-admin/src/main/java/com/epam/newsmanagement/controller/TestController.java
@@ -0,0 +1,24 @@
+package com.epam.newsmanagement.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import com.epam.newsmanagement.entity.Author;
+import com.epam.newsmanagement.exception.ServiceException;
+import com.epam.newsmanagement.service.ServiceManager;
+
+@Controller
+public class TestController {
+
+	@Autowired
+	private ServiceManager service;
+
+	@RequestMapping("/test")
+	public String test( Model model) throws ServiceException {
+		Author author = new Author();
+		author.setName("VASYA!");
+		model.addAttribute("message", service.addNewAuthor(author));
+		return "hello";
+	}
+}
