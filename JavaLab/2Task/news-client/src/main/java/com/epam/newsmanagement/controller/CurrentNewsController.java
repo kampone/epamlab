05577ebeb6f2e6006e3c20450be8e60d4e5261ca
kampone <vladislavkaminski2806@gmail.com>
2015-08-05 19:23:19 +0300

    refactor currentNewsCOntrollew

--- JavaLab/2Task/news-client/src/main/java/com/epam/newsmanagement/controller/CurrentNewsController.java
+++ JavaLab/2Task/news-client/src/main/java/com/epam/newsmanagement/controller/CurrentNewsController.java
@@ -11,6 +11,8 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 import com.epam.newsmanagement.entity.Comment;
 import com.epam.newsmanagement.entity.NewsVO;
 import com.epam.newsmanagement.entity.SearchCriteria;
@@ -25,25 +27,25 @@ public class CurrentNewsController {
 	private ServiceManager service;
 
 	@RequestMapping("/news/{index}")
-	public String getCurrentNews(HttpSession session,Model model,@PathVariable("index") Integer index) throws ServiceException {
+	public String getCurrentNews(HttpSession session, Model model, @PathVariable("index") Integer index)
+			throws ServiceException {
 		session.setAttribute("index", index);
-		prepareNewsByIndex(session, model);
-		return "current_news";
+		return "redirect:/current/see";
 	}
 
 	@RequestMapping("/next")
 	public String getNextNews(HttpSession session, Model model) throws ServiceException {
-		SearchCriteria searchCriteria=(SearchCriteria) session.getAttribute("searchCriteria");
+		SearchCriteria searchCriteria = (SearchCriteria) session.getAttribute("searchCriteria");
 		Integer index = (Integer) session.getAttribute("index");
 		++index;
 		List<NewsVO> newsVOList = service.getNewsVO(searchCriteria, index, index);
 		if (!newsVOList.isEmpty()) {
 			session.setAttribute("index", index);
-			model.addAttribute("newsVO", newsVOList.get(0));
+			prepareNewsByIndex(session, model);
 		} else {
 			--index;
 			model.addAttribute("errorMessage", "Nothing else");
-			model.addAttribute("newsVO", service.getNewsVO(searchCriteria, index, index).get(0));
+			prepareNewsByIndex(session, model);
 		}
 		return "current_news";
 	}
@@ -56,35 +58,45 @@ public class CurrentNewsController {
 		List<NewsVO> newsVOList = service.getNewsVO(searchCriteria, index, index);
 		if (!newsVOList.isEmpty()) {
 			session.setAttribute("index", index);
+			prepareNewsByIndex(session, model);
 		} else {
 			++index;
-			session.setAttribute("index", index);
 			model.addAttribute("errorMessage", "Nothing else");
+			prepareNewsByIndex(session, model);
 		}
-		return "redirect:/current/see";
+		return "current_news";
 	}
 
-	@RequestMapping("/add-comment")
-	public String addComment(HttpSession session, Model model,@Valid Comment comment,BindingResult bindingResult) throws ServiceException {
-		if(!bindingResult.hasErrors()){
+	@RequestMapping(value="/add-comment", method=RequestMethod.POST)
+	public String addComment(HttpSession session, Model model, @Valid Comment comment, BindingResult bindingResult)
+			throws ServiceException {
+		if (!bindingResult.hasErrors()) {
 			service.createComment(comment);
 		}
-		return "forward:/current/see";
+		prepareNewsByIndex(session, model);
+		return "current_news";
 	}
 	
+	@RequestMapping(value="/add-comment", method=RequestMethod.GET)
+	public String ingore(HttpSession session, Model model) throws ServiceException{
+		prepareNewsByIndex(session, model);
+		return "current_news";
+	}
+
 	private void prepareNewsByIndex(HttpSession session, Model model) throws ServiceException {
 		SearchCriteria searchCriteria = (SearchCriteria) session.getAttribute("searchCriteria");
 		int index = (int) session.getAttribute("index");
 		model.addAttribute("newsVO", service.getNewsVO(searchCriteria, index, index).get(0));
 	}
-		
-	
+
 	@RequestMapping("/see")
-	public String watchNews(HttpSession session, Model model) throws ServiceException{
+	public String watchNews(HttpSession session, Model model) throws ServiceException {
 		SearchCriteria searchCriteria = (SearchCriteria) session.getAttribute("searchCriteria");
 		int index = (int) session.getAttribute("index");
 		model.addAttribute("newsVO", service.getNewsVO(searchCriteria, index, index).get(0));
 		return "current_news";
 	}
 	
+	
+
 }
